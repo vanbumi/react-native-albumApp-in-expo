e94a9f12238287b8bd499f73dc6abdc2fce329bf
vanbumi <jsp.dio@gmail.com>
2017-07-06 11:18:11 +0700

    end of v.31 - livecycle method

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+  componentWillMount() {
+    console.log("componentWillMount works!");
+  }
+
   render () {
     return (
       <View>
-        <Text>Album List!</Text>
+        <Text>Album List ku</Text>
       </View>
     );
   }
