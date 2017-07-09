14295e07e0b01b0cbc1bdb434064eeaf4225a3dd
vanbumi <jsp.dio@gmail.com>
2017-07-09 09:12:38 +0700

    end of v.46 displaying image

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,19 +4,27 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
+  const { thumbnailContainerStyle, thumbnailStyle, headerContentStyle, headerTextStyle, imageStyle } = styles;
+
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
-            style={styles.thumbnailStyle} 
+            style={thumbnailStyle} 
             source={{ uri: props.album.thumbnail_image }
           }/>
         </View>
-        <View style={styles.headerContentStyle}>
-          <Text>{props.album.title}</Text>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{props.album.title}</Text>
           <Text>{props.album.artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image 
+          style={ imageStyle }
+          source={{ uri: props.album.image }} 
+        />    
       </CardSection>
     </Card>
   );
@@ -27,10 +35,24 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     width: 50,
     height: 50
-  }
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  } 
 };
 
 export default AlbumDetail;
